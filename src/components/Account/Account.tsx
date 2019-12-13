5a3501b83d09465b62bab6cd7ea02a8c7569c664
tataqt <tata_98@ukr.net>
2019-12-13 17:06:30 +0200

    half of work

---
+++ src/components/Account/Account.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Account extends Component {
+    public render() {
+        return (
+            <div>
+                Account : {localStorage.getItem('authUser')}
+            </div>
+        );
+    }
+}
